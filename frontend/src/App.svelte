820bdf0e0fcec580c109c7102ece603b2a7dfc2b
twil3akine <twil3akine@gmail.com>
2025-08-08 22:13:32 +0900

    feat: セットアップ完了？
    - Svelte
    - Tailwindcss, Postcss
    - Rabee UI
    が多分セットアップできた。

---
+++ frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Button from "./lib/Button.svelte";
+  import Input from "./lib/Input.svelte";
+
+  let value = $state("");
+  let isError = $derived(value === "");
+</script>
+
+<div class="flex flex-col w-full">
+  {#if isError}
+    <p class="text-destructive mb-2 text-sm">エラーメッセージです。</p>
+  {/if}
+  <Input type="text" placeholder="プレースホルダー" required {isError} bind:value />
+
+  <Button>aaa</Button>
+</div>
