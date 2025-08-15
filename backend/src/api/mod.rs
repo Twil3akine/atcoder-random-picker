6dbfffe160a46d65fc0e44656e86a40b8b573408
twil3akine <twil3akine@gmail.com>
2025-08-15 19:06:30 +0900

    feat: problems.jsonをAPIで取得可能に
    - reqwestにUA追加で起動時に取得するように変更した。

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -3,15 +3,21 @@ use std::{error::Error, marker::Sync};
 
 #[derive(Debug, Deserialize)]
 pub struct Problem {
-	pub id: String,
-	pub contest_id: String,
-	pub title: String,
+    pub id: String,
+    pub contest_id: String,
+    pub title: String,
 }
 
-pub async fn fetch_problem() -> Result<Vec<Problem>, Box<dyn Error+Send+Sync>> {
-	let url = "https://kenkoooo.com/atcoder/resources/problems.json";
-	let text = reqwest::get(url).await?.text().await?;
-	let problem: Vec<Problem> = serde_json::from_str(&text)?;
+pub async fn fetch_problem() -> Result<Vec<Problem>, Box<dyn Error + Send + Sync>> {
+    let url = "https://kenkoooo.com/atcoder/resources/problems.json";
 
-	Ok(problem)
+    // reqwest::Clientを作成し、User-Agentヘッダーを追加
+    let client = reqwest::Client::builder()
+        .user_agent("atcoder-random-picker/0.1") // 任意の名前やバージョンを指定
+        .build()?;
+
+    let text = client.get(url).send().await?.text().await?;
+    let problem: Vec<Problem> = serde_json::from_str(&text)?;
+
+    Ok(problem)
 }
