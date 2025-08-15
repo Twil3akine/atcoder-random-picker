dd838cd5847b9ce2485ef717f812af0d10f02d78
twil3akine <twil3akine@gmail.com>
2025-08-15 21:09:33 +0900

    recommit: ac1efec

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,11 +2,18 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Server};
 use std::net::SocketAddr;
 use std::convert::Infallible;
+use std::{println};
+use std::result::Result::Err;
 
-use backend::router;
+use backend::{api, router};
 
 #[tokio::main]
 async fn main() {
+  match api::fetch_problem().await {
+    Ok(_ps) => println!("Successed to fetch problems"),
+    Err(e) => eprintln!("Failed to failed problems: {}", e),
+  }
+
   let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
   let make_svc = make_service_fn(|_conn| async {
